503f375c3a7fd1cf9e5a19ac7a2c6568a57da63e
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-11-24 00:27:13 +0100

    added storage handler docs

--- src/js/storage/storage.js
+++ src/js/storage/storage.js
@@ -1,16 +1,33 @@
+/**
+ * Handles local storage
+ */
 class localStorageHandler {
+  /**
+   * Sets an item in local storage
+   * @param {string} key
+   * @param {any} value
+   */
   set = (key, value) => {
     localStorage.setItem(key, JSON.stringify(value));
   };
 
+  /**
+   * Returns an item from local storage
+   * @param {string} key
+   * @returns {any} returns parsed value from storage
+   */
   get = (key) => {
     try {
       return JSON.parse(localStorage.getItem(key));
     } catch {
-      return null;
+      return false;
     }
   };
 
+  /**
+   * Deletes an item from local storage
+   * @param {string} key
+   */
   remove = (key) => localStorage.removeItem(key);
 }
 
