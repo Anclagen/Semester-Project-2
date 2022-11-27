7fa500a3657dbed27e8932214021560459978fa8
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-11-27 15:26:15 +0100

    function to get all listings

---
+++ src/js/api/listing/getAllListings.js
@@ -0,0 +1,29 @@
+import { baseURL } from "../constants.js";
+import { myHeader } from "../header.js";
+
+/**
+ * Get all listings
+ * @param {String} sort id/title/description/tags, default set to created
+ * @param {String} order desc or asc, default set to desc
+ * @param {Number} offset use 100 to get page 2 of results, default set to 0
+ * @returns {Promise<Array>} Response array of listing objects.
+ */
+export async function getAllListings(
+  sort = "created",
+  order = "desc",
+  offset = 0
+) {
+  const url = `${baseURL}listings?_seller=true&_bids=true&offset=${offset}&sort=${sort}&sortOrder=${order}`;
+
+  const requestOptions = {
+    method: "GET",
+    headers: myHeader,
+  };
+
+  const response = await fetch(url, requestOptions);
+  if (response.ok) {
+    return await response.json();
+  }
+
+  throw new Error(response);
+}
