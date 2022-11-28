c7c0250dc75f6b3dd662688510fe5ae49c4a2986
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-11-28 21:54:30 +0100

    accordion for more media inputs

---
+++ src/js/listeners/showMoreMediaInputs.js
@@ -0,0 +1,21 @@
+import { showInputContainer } from "../tools/inputAccordion.js";
+
+/**
+ * Shows an extra media input field on create/update form
+ * @param {Click} event
+ */
+export const addMoreMedia = function (event) {
+  const mediaInputContainer = document.querySelector("#media-inputs-container");
+  //const disabledMediaInputs = mediaInputContainer.querySelectorAll("input[type=url]:disabled");
+  const disabledMediaInputs = mediaInputContainer.querySelectorAll(
+    "input[type=url].hidden"
+  );
+  const removeMediaBtn = document.querySelector("#remove-media-btn");
+
+  if (disabledMediaInputs.length === 1) {
+    event.target.classList.add("hidden");
+  }
+  removeMediaBtn.classList.remove("hidden");
+
+  showInputContainer(disabledMediaInputs[0]);
+};
