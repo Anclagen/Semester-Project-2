ba97335a8dfc0f20b2308a556e463c01275b5117
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-12-05 00:49:25 +0100

    listing page controls & pagination added

--- src/js/pages/listing.js
+++ src/js/pages/listing.js
@@ -1,17 +1,8 @@
-import { getAllListings } from "../api/listing/getAllListings.js";
-import { addLoader } from "../render/loader.js";
-import { filterActiveListings } from "../sort_search_filter/filterActiveListing.js";
-import { renderActiveListings } from "../tools/renderActiveListing.js";
+import { getParamURL } from "../tools/getParamsURL.js";
+import { showSortedListings } from "../sort_search_filter/listingPageSorting.js";
 
 export const listingPageSetup = async function () {
-  const listingsContainer = document.querySelector("#listing-results");
-  const queryString = window.location.search;
-  const params = new URLSearchParams(queryString);
-  let search = params.get("search");
-
-  const sort = params.get("sort");
-  console.log(sort);
-
+  let sort = getParamURL("sort");
   const inputs = document.querySelectorAll("input[type=radio]");
   if (sort === null || sort === "newest") {
     inputs[0].checked = true;
@@ -21,78 +12,17 @@ export const listingPageSetup = async function () {
     inputs[2].checked = true;
   }
 
-  const showSortedListings = function () {
-    const checked = document.querySelector("input[type=radio]:checked").value;
-    listingsContainer.innerHTML = "";
-    if (checked === "newest") {
-      inputs[0].checked = true;
-      renderActiveListings(listingsContainer, filteredListings, 40);
-    } else if (checked === "ends-soon") {
-      filteredListings.sort((a, b) => new Date(a.endsAt) - new Date(b.endsAt));
-      renderActiveListings(listingsContainer, listings, 40);
-      inputs[1].checked = true;
-    } else if (checked === "popular") {
-      inputs[2].checked = true;
-      const popular = filteredListings.sort(
-        (a, b) => b.bids.length - a.bids.length
-      );
-      renderActiveListings(listingsContainer, popular, 40);
-    }
-  };
+  //pagination variables
+  let page = 1;
+  let numberPages = 0;
+  let offset = 0;
 
-  const searchListings = function () {
-    console.log(search);
-    const searchTerm = search.toLowerCase();
-    const filteredListings = activeListing.filter((listing) => {
-      const seller = listing.seller.name.toLowerCase();
-      const title = listing.title.toLowerCase();
-      const body =
-        listing.description === null ? "" : listing.description.toLowerCase();
-      const tags = listing.tags.map((tag) => {
-        return tag.toLowerCase();
-      });
-
-      if (seller.startsWith(searchTerm)) {
-        return true;
-      } else if (title.indexOf(searchTerm) >= 0) {
-        return true;
-      } else if (body.indexOf(searchTerm) >= 0) {
-        return true;
-      } else {
-        let isTag = false;
-        tags.forEach((tag) => {
-          if (tag.startsWith(searchTerm)) {
-            isTag = true;
-          }
-        });
-        return isTag;
-      }
-    });
-
-    return filteredListings;
-  };
-
-  addLoader(listingsContainer);
-  let listings = [];
-  let activeListing = [];
-  let filteredListings = [];
-
-  try {
-    listings = await getAllListings();
-    activeListing = filterActiveListings(listings);
-    if (search !== null) {
-      filteredListings = searchListings();
-    } else {
-      filteredListings = activeListing;
-    }
-    showSortedListings();
-  } catch (error) {
-    console.log(error);
-    listingsContainer.innerHTML = `<div class="text-danger text-center py-3">
-                                      <p>Oops!?, an error occurred. Please refresh the page and try again. </p>
-                                    <div>`;
-  }
-
+  await showSortedListings(page, numberPages, offset);
+  // radio button field set listener to change sorting.
   const fieldset = document.querySelector("fieldset");
-  fieldset.addEventListener("change", showSortedListings);
+
+  fieldset.addEventListener("change", () => {
+    page = 1;
+    showSortedListings(page, numberPages, offset);
+  });
 };
---
+++ src/js/sort_search_filter/listingPageSorting.js
@@ -0,0 +1,108 @@
+import { getAllListings } from "../api/listing/getAllListings.js";
+import { addLoader } from "../render/loader.js";
+import { renderActiveListings } from "../tools/renderActiveListing.js";
+import { getParamURL } from "../tools/getParamsURL.js";
+import { searchListings } from "./search.js";
+import { sortListings } from "./sort.js";
+import { addListingPageControls } from "../listeners/addListingPageControls.js";
+
+let listings = [];
+let sortedActiveListings = [];
+let filteredListings = [];
+
+/**
+ * Fetches depending sort/search query and generates content for page
+ * @param {Number} page current page number
+ * @param {Number} numberPages Total pages, is updated as more fetches are performed
+ * @param {Number} offset offset to get more results from api as needed
+ */
+export const showSortedListings = async function (
+  page,
+  numberPages,
+  offset = 0
+) {
+  try {
+    const inputs = document.querySelectorAll("input[type=radio]");
+    const listingsContainer = document.querySelector("#listing-results");
+    listingsContainer.innerHTML = "";
+    addLoader(listingsContainer);
+
+    // filter results for search if querystring present
+    const search = getParamURL("search");
+    if (search !== null) {
+      if (page === 1) {
+        listings = [
+          ...(await getAllListings()),
+          ...(await getAllListings("created", "desc", 100)),
+        ];
+        filteredListings = searchListings([...listings]);
+        sortedActiveListings = sortListings(filteredListings);
+        numberPages = Math.ceil(filteredListings.length / 20);
+      }
+    } else if (inputs[2].checked) {
+      //checks if popular radio option checked fetches listings based on that.
+      if (page === 1) {
+        listings = [
+          ...(await getAllListings()),
+          ...(await getAllListings("created", "desc", 100)),
+        ];
+      }
+      sortedActiveListings = sortListings(listings);
+    } else if (inputs[1].checked) {
+      //checks if ending soon radio option checked fetches listings based on that.
+      if (page === 1) {
+        listings = await getAllListings("endsAt", "asc");
+      }
+
+      if (page === numberPages - 1 && listings.length % 100 === 0) {
+        offset += 100;
+        listings = [
+          ...listings,
+          ...(await getAllListings("endsAt", "asc", offset)),
+        ];
+
+        numberPages = Math.ceil(listings.length / 20);
+      }
+      sortedActiveListings = listings;
+    } else if (inputs[0].checked) {
+      //checks if newest radio option checked fetches listings based on that.
+      if (page === 1) {
+        listings = await getAllListings("created", "desc", offset);
+      }
+      if (page === numberPages - 1 && listings.length % 100 === 0) {
+        offset += 100;
+        listings = [
+          ...listings,
+          ...(await getAllListings("created", "desc", offset)),
+        ];
+        numberPages = Math.ceil(listings.length / 20);
+      }
+      sortedActiveListings = listings;
+    }
+
+    listingsContainer.innerHTML = "";
+    renderActiveListings(
+      listingsContainer,
+      [...sortedActiveListings].slice((page - 1) * 20),
+      20
+    );
+
+    if (page === 1 && search === null) {
+      numberPages = Math.ceil(listings.length / 20);
+    }
+
+    if (sortedActiveListings.length === 0 && listings.length === 0) {
+      listingsContainer.innerHTML = `<div class="bg-secondary text-center p-3 mt-2">
+                                        <p>No listings found.</p>
+                                    <div>`;
+    }
+
+    addListingPageControls(page, numberPages, offset, listings);
+  } catch (error) {
+    console.log(error);
+    const listingsContainer = document.querySelector("#listing-results");
+    listingsContainer.innerHTML = `<div class="text-danger text-center py-3">
+                                      <p>Oops!?, an error occurred. Please refresh the page and try again. </p>
+                                    <div>`;
+  }
+};
