68309d1820485d7b68709248520b5c17bcfa7480
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-12-07 00:29:10 +0100

    error handling for bidding and deleting

--- src/js/listeners/listing/placeBid.js
+++ src/js/listeners/listing/placeBid.js
@@ -1,12 +1,17 @@
 import { placeABid } from "../../api/listing/placeBid.js";
 
 export const placeBidFormListener = async function (event) {
-  event.preventDefault();
-  const queryString = window.location.search;
-  const params = new URLSearchParams(queryString);
-  let id = params.get("id");
-  console.log(event.target.amount.value);
-  const response = await placeABid(id, Number(event.target.amount.value));
-  console.log(response);
-  location.reload();
+  try {
+    event.preventDefault();
+    const queryString = window.location.search;
+    const params = new URLSearchParams(queryString);
+    let id = params.get("id");
+    await placeABid(id, Number(event.target.amount.value));
+    location.reload();
+  } catch (error) {
+    console.log(error);
+    const errorContainer = document.querySelector("#error-reporting-container");
+    errorContainer.innerHTML = `<p class="p-3 text-losing bg-secondary">An error occurred. Listing may have finished or no longer exists. </p>`;
+    location.hash = "#error-reporting-container";
+  }
 };
