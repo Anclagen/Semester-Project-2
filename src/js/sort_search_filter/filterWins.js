1abd5cbcda43a30941482a28569cc8d15cd7c922
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-12-12 16:31:14 +0100

    sorts and filters for duplicates on profile page data

---
+++ src/js/sort_search_filter/filterWins.js
@@ -0,0 +1,26 @@
+export const filterWonListings = function (profile, bids) {
+  const wins = profile.wins;
+
+  let bidOnListings = [];
+  let duplicatesIds = [];
+
+  //sort to make sure top bids go in first.
+  bids.sort((a, b) => b.amount - a.amount);
+
+  bids.forEach((bid) => {
+    bidOnListings.push({ ...bid.listing, bids: [{ amount: bid.amount }] });
+  });
+
+  const uniqueListings = bidOnListings.filter((element) => {
+    if (!duplicatesIds.includes(element.id)) {
+      duplicatesIds.push(element.id);
+      return true;
+    }
+  });
+
+  const wonListings = uniqueListings.filter((listing) =>
+    wins.includes(listing.id)
+  );
+  wonListings.sort((a, b) => new Date(b.endsAt) - new Date(a.endsAt));
+  return wonListings;
+};
