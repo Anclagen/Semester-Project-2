1ccf3ee65b34721a70bcc2faeaec90a7d1f34e7a
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-12-12 16:35:07 +0100

    profile page setup function updates

--- src/js/pages/profile.js
+++ src/js/pages/profile.js
@@ -1,16 +1,20 @@
 import { getUserProfile } from "../api/profile/getProfile.js";
+import { getUserProfileBids } from "../api/profile/getProfileBids.js";
 import { getUserProfileListings } from "../api/profile/getProfileListings.js";
 import { showSensitiveDetails } from "../interface/isUserProfile.js";
 import { setupAvatarPreview } from "../listeners/profile/avatar-preview.js";
 import { updateAvatarListener } from "../listeners/profile/updateAvatar.js";
 import { createListingCard } from "../render/listingCard.js";
 import { updateProfileDetails } from "../render/updateProfileDetails.js";
+import { sortFilterBidOn } from "../sort_search_filter/filterDuplicateListings.js";
+import { filterWonListings } from "../sort_search_filter/filterWins.js";
 import { storage } from "../storage/storage.js";
+import { recentProfileCardsSetup } from "../tools/fillRecentProfileCards.js";
+import { getParamURL } from "../tools/getParamsURL.js";
 
 export const profileSetup = async function () {
-  const queryString = window.location.search;
-  const params = new URLSearchParams(queryString);
-  let user = params.get("user");
+  let user = getParamURL("user");
+
   if (user === null || user === storage.get("profile").name) {
     user = storage.get("profile").name;
     showSensitiveDetails();
@@ -18,14 +22,57 @@ export const profileSetup = async function () {
     updateForm.addEventListener("submit", updateAvatarListener);
     setupAvatarPreview();
   }
-  const profile = await getUserProfile(user);
-  updateProfileDetails(profile);
 
-  const profileListings = await getUserProfileListings(user);
-  const yourListingsContainer = document.querySelector(
-    "#your-listings-container"
-  );
-  await profileListings.map((listing) => {
-    yourListingsContainer.append(createListingCard(listing));
-  });
+  if (getParamURL("deleted")) {
+    const deleteSuccessContainer = document.querySelector("#delete-success");
+    deleteSuccessContainer.innerHTML = `<p class=" text-center p-2 my-2 rounded-2 text-winning bg-secondary">Listing Was Deleted.</p>`;
+  }
+  try {
+    const profile = await getUserProfile(user);
+    updateProfileDetails(profile);
+    const profileBids = await getUserProfileBids(user);
+    const uniqueListings = sortFilterBidOn(profileBids);
+    if (uniqueListings.length > 0) {
+      const yourBidsContainer = document.querySelector(
+        "#ongoing-bids-container"
+      );
+      yourBidsContainer.innerHTML = "";
+      uniqueListings.map((listing) => {
+        const card = createListingCard(listing.card);
+        card.querySelector(
+          ".listing-price"
+        ).innerHTML = `Your Bid: £${listing.card.bids.amount}.00`;
+        yourBidsContainer.append(card);
+      });
+    }
+    // most recent and all listings
+    const profileListings = await getUserProfileListings(user);
+    const yourListingsContainer = document.querySelector(
+      "#your-listings-container"
+    );
+    const newListingsContainer = document.querySelector(
+      "#newest-listings-container"
+    );
+    recentProfileCardsSetup(
+      profileListings,
+      newListingsContainer,
+      yourListingsContainer,
+      "User hasn't created any listings."
+    );
+
+    // most recent and all wins
+    const wonListings = filterWonListings(profile, profileBids);
+    const yourWinsContainer = document.querySelector("#wins-container");
+    const recentWinsContainer = document.querySelector(
+      "#recent-wins-container"
+    );
+    recentProfileCardsSetup(
+      wonListings,
+      recentWinsContainer,
+      yourWinsContainer,
+      "No wins yet."
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
---
+++ src/js/tools/fillRecentProfileCards.js
@@ -0,0 +1,29 @@
+import { createListingCard } from "../render/listingCard.js";
+
+export const recentProfileCardsSetup = function (
+  data,
+  recent,
+  all,
+  message = "No listings yet."
+) {
+  if (data.length > 0) {
+    all.innerHTML = "";
+    recent.innerHTML = "";
+    data.map((listing) => {
+      all.append(createListingCard(listing));
+    });
+
+    data.forEach((listing, index) => {
+      if (index < 3) {
+        const card = createListingCard(listing);
+        card.classList = `p-2 p-xl-3 col-lg-6 col-xl-4 listing-card ${
+          index > 0 ? "d-none d-xl-flex" : ""
+        } ${index === 1 ? "d-lg-flex" : ""}`;
+        recent.append(card);
+      }
+    });
+  } else {
+    all.innerHTML = ` <p class="ps-4">${message}</p>`;
+    recent.innerHTML = ` <p class="ps-4">${message}</p>`;
+  }
+};
